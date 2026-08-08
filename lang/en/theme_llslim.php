6b54cdf1f2dc4235858448d1e62241f47c018483
Kevin Williams <llslim@gmail.com>
2026-08-07 23:56:03 -0400

    Initial commit of theme_llslim Moodle theme module

---
+++ lang/en/theme_llslim.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings for theme_llslim.
+ *
+ * @package    theme_llslim
+ * @copyright  2026 LL Slim
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'LL Slim Theme';
+$string['choosereadme'] = 'theme_llslim is a modern, accessible theme designed for LL Slim with full WCAG 2.2 Level AA compliance.';
+$string['region-side-pre'] = 'Right';
---
+++ lib.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lib functions for theme_llslim.
+ *
+ * @package    theme_llslim
+ * @copyright  2026 LL Slim
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Inject theme_llslim extra SCSS/CSS rules.
+ *
+ * @param theme_config $theme The theme config object.
+ * @return string SCSS / CSS.
+ */
+function theme_llslim_get_extra_scss($theme) {
+    return '';
+}
